67625999f4a4fd003305b97f2a7ba1204354df08
NITHIN M C <mcnithin372@gmail.com>
2024-06-29 19:56:02 +0530

    Started the Sorting Techniques

--- src/Striver's A2Z Sheet/Sorting/main.py
+++ src/Striver's A2Z Sheet/Sorting/main.py
@@ -1,17 +1,34 @@
 n = int(input("\nEnter the Size of the Array : "))
-a = list(map(int,input("\n Enter the Array of Elements : ").split()))
+a = list(map(int, input("\n Enter the Array of Elements : ").split()))
+
 
 class Sorting:
     def __init__(self):
-        print("\n Learning the Sorting \n")
-        
-    def bubbleSort(self,n,a):
-        for i in range(len(n)):
+        print("\n")
+
+    def bubbleSort(self, n, a):
+        print("\nExecuting Bubble sort")
+
+        for i in range(n):
             for j in range(i):
-                if a[i] > a[j]:
-                    (a[i],a[j]) = (a[j],a[i])
+                if a[i] < a[j]:
+                    (a[i], a[j]) = (a[j], a[i])
         return a
-    
-    
-result = Sorting().bubbleSort(n,a)
-print("\n The Elements after Sorting is : ",result)
+
+    def selectionSort(self, n, a):
+        print("\nExecuting Selection sort")
+
+        for i in range(n-1):
+            min = i
+            for j in range(i, n, 1):
+                if a[j] < a[min]:
+                    min = j
+            (a[i], a[min]) = (a[min], a[i])
+
+        return a
+
+
+result = Sorting().selectionSort(n, a)
+print("\n The Elements after Sorting is : ", result)
+
+# 13 46 24 52 20 9
